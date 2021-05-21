3b0c0471f2444772b3859b3890b67053cfcaadcf
jmatak <josip.matak@hotmail.com>
2021-05-21 15:57:28 +0200

    Update license for source files

--- memgraph/query_builder.py
+++ memgraph/query_builder.py
@@ -1,9 +1,22 @@
-from abc import abstractmethod, ABC
-from typing import Any, Dict, Iterator, List, Optional, Union
+# Copyright (c) 2016-2021 Memgraph Ltd. [https://memgraph.com]
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import re
+from abc import ABC, abstractmethod
+from typing import Any, Dict, Iterator, List, Optional, Union
 
-from .memgraph import Memgraph, Connection
+from .memgraph import Connection, Memgraph
 from .utilities import to_cypher_labels, to_cypher_properties, to_cypher_value
 
 
