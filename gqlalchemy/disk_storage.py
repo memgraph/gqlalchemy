85aaa980cbe5b0f5994865e4842481cf4ddf3df9
MasterMedo <mislav.vuletic@gmail.com>
2022-01-03 08:53:01 +0100

    add basic sqlite functionality

--- gqlalchemy/disk_storage.py
+++ gqlalchemy/disk_storage.py
@@ -1,18 +1,21 @@
 import sqlite3
-from typing import Any
+import contextlib
+
+from typing import Any, List
 
 
 class OnDiskPropertyDatabase:
-    def saveNodeProperty(node_id: int, name: str, value: Any):
+    def save_node_property(node_id: int, name: str, value: Any):
+        self.execute_query("SELECT ")
         pass
 
-    def loadNodeProperty(node_id: int, name: str, value: Any):
+    def load_node_property(node_id: int, name: str, value: Any):
         pass
 
-    def saveRelationshipProperty(relationship_id: int, name: str, value: Any):
+    def save_relationship_property(relationship_id: int, name: str, value: Any):
         pass
 
-    def loadRelationshipProperty(relationship_id: int, name: str, value: Any):
+    def load_relationship_property(relationship_id: int, name: str, value: Any):
         pass
 
 
@@ -20,14 +23,30 @@ class SqliteDatabase(OnDiskDatabase):
     def __init__(self, database: str):
         pass
 
-    def saveNodeProperty(node_id: int, name: str, value: Any):
+    def execute_query(query) -> List[Any]:
+        with contextlib.closing(sqlite3.connect(self.database_name)) as conn:
+            with conn:  # autocommit changes
+                with contextlib.closing(conn.cursor()) as cursor:
+                    cursor.execute(query)
+                    return cursor.fetchall()
+
+    def create_node_property_table():
+        # node_id, property_name, property_value
         pass
 
-    def loadNodeProperty(node_id: int, name: str, value: Any):
+    def create_relationship_property_table():
+        # relationship_id, property_name, property_value
         pass
 
-    def saveRelationshipProperty(relationship_id: int, name: str, value: Any):
+    def save_node_property(node_id: int, name: str, value: Any):
+        self.execute_query("SELECT ")
         pass
 
-    def loadRelationshipProperty(relationship_id: int, name: str, value: Any):
+    def load_node_property(node_id: int, name: str, value: Any):
+        pass
+
+    def save_relationship_property(relationship_id: int, name: str, value: Any):
+        pass
+
+    def load_relationship_property(relationship_id: int, name: str, value: Any):
         pass
