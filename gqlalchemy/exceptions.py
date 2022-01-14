4ac9eb808fb30f9dfc8158b9adffd908a77b5cc8
MasterMedo <mislav.vuletic@gmail.com>
2022-01-14 09:05:20 +0100

    change capitalisation on global variable to UPPER_CASE

--- gqlalchemy/exceptions.py
+++ gqlalchemy/exceptions.py
@@ -1,10 +1,10 @@
-DatabaseMissingInFieldErrorMessage = """
+DATABASE_MISSING_IN_FIELD_ERROR_MESSAGE = """
 Can't have an index on a property without providing the database `db` object.
 Define your property as:
   {field}: {field_type} = Field({constraint}=True, db=GraphDatabase())
 """
 
-SubclassNotFoundWarning = """
+SUBCLASS_NOT_FOUND_WARNING = """
 GraphObject subclass(es) '{types}' not found.
 '{cls.__name__}' will be used until you create a subclass.
 """
@@ -16,7 +16,7 @@ class GQLAlchemyWarning(Warning):
 
 class GQLAlchemySubclassNotFoundWarning(GQLAlchemyWarning):
     def __init__(self, types, cls):
-        self.message = SubclassNotFoundWarning.format(types=types, cls=cls)
+        self.message = SUBCLASS_NOT_FOUND_WARNING.format(types=types, cls=cls)
 
 
 class GQLAlchemyError(Exception):
@@ -29,7 +29,7 @@ class GQLAlchemyUniquenessConstraintError(GQLAlchemyError):
 
 class GQLAlchemyDatabaseMissingInFieldError(GQLAlchemyError):
     def __init__(self, constraint, field, field_type):
-        self.message = DatabaseMissingInFieldErrorMessage.format(
+        self.message = DATABASE_MISSING_IN_FIELD_ERROR_MESSAGE.format(
             constraint=constraint,
             field=field,
             field_type=field_type,
