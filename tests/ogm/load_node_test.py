1f04663c4f3ffd3b1825a75bb79a41770b9c0429
katarinasupe <supe.katarina@gmail.com>
2022-01-26 09:33:44 +0100

    Add load node test

---
+++ tests/ogm/load_node_test.py
@@ -0,0 +1,22 @@
+from gqlalchemy import Node, Field, Memgraph
+
+
+memgraph = Memgraph()
+
+class User(Node):
+    name: str = Field(index=True, exists=True, unique=True, db=memgraph)
+
+class Streamer(Node, labels={"User", "Stream"}):
+    name: str = Field(index=True, unique=True, db=memgraph, label="User")
+    id: str = Field(index=True, unique=True, db=memgraph)
+    followers: int = Field()
+    totalViewCount: int = Field()
+
+def test_load_node():
+    streamer = Streamer(name="Mislav", id="7", followers=777, totalViewCount=7777).save(memgraph)
+    loaded_streamer = memgraph.load_node(Node(id="7", _label="Stream"))
+    assert loaded_streamer.name == "Mislav"
+    assert loaded_streamer.id == "7"
+    assert loaded_streamer.followers == 777
+    assert loaded_streamer.totalViewCount == 7777
+    assert loaded_streamer._labels == {"Streamer", "User"}
