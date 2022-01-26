716dc4185d1ab15e058883967e2815ddbc0e1cb1
katarinasupe <supe.katarina@gmail.com>
2022-01-26 12:18:10 +0100

    Minor fixes

--- tests/ogm/load_node_test.py
+++ tests/ogm/load_node_test.py
@@ -3,17 +3,20 @@ from gqlalchemy import Node, Field, Memgraph
 
 memgraph = Memgraph()
 
+
 class User(Node):
     name: str = Field(index=True, exists=True, unique=True, db=memgraph)
 
-class Streamer(Node, labels={"User", "Stream"}):
-    name: str = Field(index=True, unique=True, db=memgraph, label="User")
+
+class Streamer(User):
+    name: str = Field(index=True, unique=True, db=memgraph)
     id: str = Field(index=True, unique=True, db=memgraph)
     followers: int = Field()
     totalViewCount: int = Field()
 
+
 def test_load_node():
-    streamer = Streamer(name="Mislav", id="7", followers=777, totalViewCount=7777).save(memgraph)
+    streamer = Streamer(name="Mislav", id="7", followers=777, totalViewCount=7777).save(memgraph)  # noqa F401
     loaded_streamer = memgraph.load_node(Node(id="7", _label="Stream"))
     assert loaded_streamer.name == "Mislav"
     assert loaded_streamer.id == "7"
