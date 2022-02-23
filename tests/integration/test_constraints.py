a4fbf64fdb0bf432ab62069ce66fda8eadab6f7a
Ivan Despot <ivan.g.despot@outlook.com>
2022-02-23 07:21:53 +0100

    Remove print()

--- tests/integration/test_constraints.py
+++ tests/integration/test_constraints.py
@@ -40,7 +40,6 @@ def test_exists_attr(memgraph_without_dropping_constraints):
         MemgraphConstraintExists("Person", "nationality"),
     }
     actual_exists_constraints = memgraph_without_dropping_constraints.get_exists_constraints()
-    print(actual_exists_constraints)
     assert set(actual_exists_constraints) == exists_constraints
 
 
