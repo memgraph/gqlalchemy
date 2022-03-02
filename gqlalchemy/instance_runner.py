37af9ddb141b6d939f64652212a227fdbaaa698a
Ivan Despot <ivan.g.despot@outlook.com>
2022-03-02 07:24:53 +0100

    Update Docker runner

--- gqlalchemy/instance_runner.py
+++ gqlalchemy/instance_runner.py
@@ -10,8 +10,10 @@
 # licenses/APL.txt.
 
 import docker
+import os
 import subprocess
 import time
+import signal
 import socket
 from abc import ABC, abstractmethod
 from typing import Dict, Union, Optional
@@ -22,7 +24,6 @@ from enum import Enum
 class DockerImage(Enum):
     MEMGRAPH = "memgraph/memgraph"
     MAGE = "memgraph/memgraph-mage"
-    PLATFORM = "memgraph/memgraph-platform"
 
 
 def wait_for_port(host: str = "127.0.0.1", port: int = 7687, delay: float = 0.01, timeout: float = 5.0):
@@ -38,11 +39,6 @@ def wait_for_port(host: str = "127.0.0.1", port: int = 7687, delay: float = 0.01
                 raise TimeoutError(
                     f"Waited too long for the port {port} on host {host} to start accepting connections."
                 ) from ex
-
-
-def is_port_in_use(port: int = 7687) -> bool:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        return s.connect_ex(("localhost", port)) == 0
 
 
 class MemgraphInstance(ABC):
@@ -97,10 +93,9 @@ class MemgraphInstanceBinary(MemgraphInstance):
 
         args_mg = f"{self.binary_path }" + (" ").join([f"{k} {v}" for k, v in self.config])
         args_mg += f" --bolt-address {self.host} --bolt-port {self.port}"
-        args_mg = (f"sudo -H -u {self.user} " if self.user != "" else "") + args_mg
+        args_mg = (f"sudo -u {self.user} " if self.user != "" else "") + args_mg
         print(args_mg)
-        # TODO: Process should be started as user memgraph
-        self.proc_mg = subprocess.Popen(args_mg, shell=True)
+        self.proc_mg = subprocess.Popen(args_mg, shell=True, preexec_fn=os.setsid)
         wait_for_port(self.host, self.port)
 
         self.connect()
@@ -109,9 +104,8 @@ class MemgraphInstanceBinary(MemgraphInstance):
     def stop(self):
         if not self.is_running():
             return
-        self.proc_mg.terminate()
-        code = self.proc_mg.wait()
-        print(f"Exit code: {code}")
+        os.killpg(os.getpgid(self.proc_mg.pid), signal.SIGTERM)
+        self.proc_mg.wait()
 
     def is_running(self):
         if self.proc_mg is None:
@@ -135,7 +129,9 @@ class MemgraphInstanceDocker(MemgraphInstance):
         self.stop()
 
         self.container = self.client.containers.run(
-            self.docker_image.value + ":" + self.docker_tag, detach=True, ports={f"7687/tcp": self.port}
+            self.docker_image.value + ":" + self.docker_tag + (" ").join([f"{k} {v}" for k, v in self.config]),
+            detach=True,
+            ports={f"7687/tcp": self.port},
         )
         wait_for_port(self.host, self.port)
 
