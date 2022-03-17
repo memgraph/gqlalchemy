e817e3cff6f133c1034ea26e881b27b541f2ad32
Bruno Sacaric <bruno.sacaric@gmail.com>
2022-03-17 16:08:01 +0100

    refactoring loaders.py

--- gqlalchemy/loaders.py
+++ gqlalchemy/loaders.py
@@ -12,29 +12,36 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from multiprocessing.sharedctypes import Value
+
 from . import Memgraph
 from .query_builder import QueryBuilder, Unwind
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from gqlalchemy.models import (
-    MemgraphIndex, 
-    MemgraphTrigger, 
-    TriggerEventObject, 
-    TriggerEventType, 
+    MemgraphIndex,
+    MemgraphTrigger,
+    TriggerEventObject,
+    TriggerEventType,
     TriggerExecutionPhase
 )
 from pyarrow import fs
 from typing import (
-    List, 
-    Dict, 
-    Any, 
+    List,
+    Dict,
+    Any,
     Optional,
-    Tuple, 
+    Tuple,
     Union
 )
 import pyarrow.dataset as ds
 import adlfs
 
+
+NAME_MAPPINGS_KEY = "name_mappings"
+ONE_TO_MANY_RELATIONS_KEY = "nx1_relations"
+MANY_TO_MANY_RELATIONS_KEY = "mxn_relations"
+INDICES_KEY = "indices"
 
 @dataclass(frozen=True)
 class ForeignKeyMapping:
@@ -69,9 +76,79 @@ class TableMapping:
     indices: Optional[List[str]] = None
 
 
-class DataSource(ABC):
-    def __init__(self, file_extension: str) -> None:
-        self._file_extension: str = file_extension
+@dataclass(frozen=True)
+class Configuration:
+    label: Optional[str] = None
+    column_names_mapping: Optional[Dict[str, str]] = None
+
+
+def get_data_loader(file_extension, filesystem_type, **kwargs):
+    if (file_extension == "parquet" or
+        file_extension == "csv" or
+        file_extension == "orc" or
+        file_extension == "ipc" or
+        file_extension == "feather" or
+        file_extension == "arrow"):
+        return PyarrowDataLoader(
+            file_extension=file_extension,
+            file_system_handler=get_filesystem(filesystem_type, **kwargs)
+        )
+    else:
+        raise ValueError(file_extension)
+
+
+def get_filesystem(filesystem_type, **kwargs):
+    if filesystem_type == "S3":
+        return S3FileSystemHandler(**kwargs)
+    elif filesystem_type == "AzureBlob":
+        return AzureBlobFileSystemHandler(**kwargs)
+    else:
+        return ValueError(filesystem_type)
+
+
+class FileSystemHandler(ABC):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @abstractmethod
+    def get_path(self):
+        pass
+
+
+class S3FileSystemHandler(FileSystemHandler):
+    def __init__(self, **kwargs):
+        self.fs = fs.S3FileSystem(
+            region=kwargs.get('s3_region'),
+            access_key=kwargs.get('s3_access_key'),
+            secret_key=kwargs.get('s3_secret_key'),
+            session_token=kwargs.get('s3_session_token', None),
+        )
+        self._bucket_name = kwargs.get('bucket_name')
+
+    def get_path(self, collection_name, file_extension):
+        return f"{self._bucket_name}/{collection_name}.{file_extension}"
+
+
+class AzureBlobFileSystemHandler(FileSystemHandler):
+    def __init__(self, **kwargs):
+        self.fs = adlfs.AzureBlobFileSystem(
+            account_name=kwargs.get('blob_account_name'),
+            account_key=kwargs.get('blob_account_key', None),
+            sas_token=kwargs.get('blob_sas_token', None)
+        )
+        self._container_name = kwargs['container_name']
+
+    def get_path(self, collection_name, file_extension):
+        return f"{self._container_name}/{collection_name}.{file_extension}"
+
+
+class DataLoader(ABC):
+    def __init__(
+        self,
+        file_system_handler: FileSystemHandler
+    ) -> None:
+        super().__init__()
+        self._file_system_handler = file_system_handler
 
     @abstractmethod
     def load_data(
@@ -82,84 +159,34 @@ class DataSource(ABC):
         pass
 
 
-class S3DataSource(DataSource):
+class PyarrowDataLoader(DataLoader):
+    """
+    Pyarrow currently supports "parquet", "ipc"/"arrow"/"feather", "csv",
+    and "orc", see pyarrow.dataset.dataset for up-to-date info.
+    ds.dataset in load_data accepts any fsspec subclass, making this DataLoader
+    compatible with fsspec-compatible filesystems.
+    """
     def __init__(
-        self, 
-        bucket_name: str,
-        s3_access_key: str,
-        s3_secret_key: str,
-        s3_region: str,
+        self,
         file_extension: str,
-        s3_session_token: Optional[str] = None,
+        file_system_handler: FileSystemHandler,
     ) -> None:
-        super().__init__(file_extension=file_extension)
-        self._bucket_name: str = bucket_name
-        self._s3_access_key: str = s3_access_key
-        self._s3_secret_key: str = s3_secret_key
-        self._s3_region: str = s3_region
-        self._s3_session_token: Optional[str] = s3_session_token
+        super().__init__(file_system_handler=file_system_handler)
+        self._file_extension = file_extension
 
     def load_data(
         self,
         collection_name: str,
         is_cross_table: bool = False,
-        columns: Optional[List[str]] = None,
+        columns: Optional[List[str]] = None
     ) -> None:
-        s3 = fs.S3FileSystem(
-            region=self._s3_region,
-            access_key=self._s3_access_key,
-            secret_key=self._s3_secret_key,
-            session_token=self._s3_session_token,
-        )
-        source = f"{self._bucket_name}/{collection_name}.{self._file_extension}"
-        print("Loading " + ("cross table " if is_cross_table else "") + f"data from {source}")
-
-        # Load dataset via Pyarrow
-        dataset = ds.dataset(
-            source=source, 
-            filesystem=s3
-        )
-
-        # Load batches from raw data
-        for batch in dataset.to_batches(
-            columns=columns,
-        ):
-            for batch in batch.to_pylist():
-                yield batch
-
-
-class AzureBlobDataSource(DataSource):
-    def __init__(
-        self,
-        container_name: str,
-        account_name: str,
-        account_key: str,
-        file_extension: str,
-        sas_token: Optional[str],
-    ) -> None:
-        super().__init__(file_extension=file_extension)
-        self._container_name = container_name
-        self._account_name = account_name
-        self._account_key = account_key
-        self._sas_token = sas_token
-
-    def load_data(
-        self,
-        collection_name: str,
-        is_cross_table: bool = False,
-        columns: Optional[List[str]] = None,
-    ) -> None:
-        azureBlob = adlfs.AzureBlobFileSystem(
-            account_name=self._account_name,
-            account_key=self._account_key,
-        )
-        source = f"{self._container_name}/{collection_name}.{self._file_extension}"
+        source = self._file_system_handler.get_path(collection_name, self._file_extension)
         print("Loading " + ("cross table " if is_cross_table else "") + f"data from {source}")
 
         # Load dataset via Pyarrow
         dataset = ds.dataset(
             source=source,
-            filesystem=azureBlob
+            filesystem=self._file_system_handler.fs
         )
 
         # Load batches from raw data
@@ -168,12 +195,6 @@ class AzureBlobDataSource(DataSource):
         ):
             for batch in batch.to_pylist():
                 yield batch
-
-
-@dataclass(frozen=True)
-class Configuration:
-    label: Optional[str] = None
-    column_names_mapping: Optional[Dict[str, str]] = None
 
 
 class TableToGraphImporter:
@@ -184,12 +205,15 @@ class TableToGraphImporter:
 
     def __init__(
         self,
-        data_source: DataSource,
-        data_configuration: Dict[str, Any], 
-        memgraph: Optional[Memgraph] = None
+        file_extension: str,
+        file_system: str,
+        data_configuration: Dict[str, Any],
+        memgraph: Optional[Memgraph] = None,
+        **kwargs
     ) -> None:
+        print(kwargs)
+        self._data_loader: DataLoader = get_data_loader(file_extension, file_system, **kwargs)
         self._memgraph: Memgraph = memgraph if memgraph is not None else Memgraph()
-        self._data_source: DataSource = data_source
 
         self.__load_configuration(data_configuration=data_configuration)
 
@@ -208,9 +232,9 @@ class TableToGraphImporter:
     def _load_nodes(self) -> None:
         for one_to_many_mapping in self._one_to_many_mappings:
             collection_name = one_to_many_mapping.table_name
-            for row in self._data_source.load_data(collection_name=collection_name):
+            for row in self._data_loader.load_data(collection_name=collection_name):
                 self._save_row_as_node(
-                    label=collection_name, 
+                    label=collection_name,
                     row=row
                 )
 
@@ -229,7 +253,7 @@ class TableToGraphImporter:
             new_property1 = self._get_property_name(collection_name=table_name1, original_column_name=property1)
             new_property2 = self._get_property_name(collection_name=table_name2, original_column_name=property2)
 
-            for row in self._data_source.load_data(collection_name=collection_name, is_cross_table=True):
+            for row in self._data_loader.load_data(collection_name=collection_name, is_cross_table=True):
                 self._save_row_as_relationship(
                     relations=[node1, node2],
                     on_properties=[(property1, new_property1), (property2, new_property2)],
@@ -242,33 +266,33 @@ class TableToGraphImporter:
             label1 = self._get_node_name(original_name=one_to_many_mapping.table_name)
             for mapping in one_to_many_mapping.mapping:
                 property1 = self._get_property_name(
-                    collection_name=one_to_many_mapping.table_name, 
+                    collection_name=one_to_many_mapping.table_name,
                     original_column_name=mapping.mapping.foreign_key
                 )
                 label2 = self._get_node_name(
                     original_name=mapping.mapping.reference_table
                 )
                 property2 = self._get_property_name(
-                    collection_name=one_to_many_mapping.table_name, 
+                    collection_name=one_to_many_mapping.table_name,
                     original_column_name=mapping.mapping.reference_key
                 )
                 edge_type = mapping.mapping.label
                 from_entity = mapping.from_entity
 
                 self._create_trigger(
-                    label1=label1, 
-                    label2=label2, 
-                    property1=property1, 
-                    property2=property2, 
-                    edge_type=edge_type, 
+                    label1=label1,
+                    label2=label2,
+                    property1=property1,
+                    property2=property2,
+                    edge_type=edge_type,
                     from_entity=from_entity
                 )
                 self._create_trigger(
-                    label1=label2, 
-                    label2=label1, 
-                    property1=property2, 
-                    property2=property1, 
-                    edge_type=edge_type, 
+                    label1=label2,
+                    label2=label1,
+                    property1=property2,
+                    property2=property1,
+                    edge_type=edge_type,
                     from_entity=not from_entity
                 )
 
@@ -295,7 +319,7 @@ class TableToGraphImporter:
             collection_name = self._get_node_name(original_name=one_to_many_mapping.table_name)
             for index in one_to_many_mapping.indices:
                 new_index = self._get_property_name(
-                    collection_name=one_to_many_mapping.table_name, 
+                    collection_name=one_to_many_mapping.table_name,
                     original_column_name=index
                 )
                 self._memgraph.create_index(index=MemgraphIndex(collection_name, new_index))
@@ -316,21 +340,21 @@ class TableToGraphImporter:
                 .to(edge_label=edge_type) \
                 .node(variable=to_node) \
                 .construct_query()
-            
+
     def _save_row_as_node(self, label: str, row: Dict[str, Any]):
         list(
             QueryBuilder(connection=self._memgraph).
             create().
-            node(labels=self._get_node_name(label), 
+            node(labels=self._get_node_name(label),
                  **{self._get_property_name(label, k): v for k, v in row.items()})
             .execute())
 
     def _save_row_as_relationship(
-        self, 
-        relations: List[str], 
-        on_properties: List[Tuple[str, str]], 
-        relation_label: str, 
-        row: Dict[str, Any], 
+        self,
+        relations: List[str],
+        on_properties: List[Tuple[str, str]],
+        relation_label: str,
+        row: Dict[str, Any],
         from_first: bool = True
     ):
         if len(relations) != len(on_properties):
@@ -340,7 +364,7 @@ class TableToGraphImporter:
         label1, label2 = relations[0], relations[1]
         property1, new_property1 = on_properties[0]
         property2, new_property2 = on_properties[1]
-        
+
         query_builder = QueryBuilder(connection=self._memgraph)
         list(
             query_builder.match()
@@ -373,30 +397,31 @@ class TableToGraphImporter:
 
         return new_col_name if new_col_name is not None else original_column_name
 
+
     def __load_configuration(self, data_configuration: Dict[str, Any]):
-        self.__load_name_mappings(data_configuration["name_mappings"])
-        self.__load_one_to_many_mappings_and_indices(data_configuration["nx1_relations"], data_configuration["indices"])
-        self.__load_many_to_many_mappings(data_configuration["mxn_relations"])
+        self.__load_name_mappings(data_configuration.get(NAME_MAPPINGS_KEY, {}))
+        self.__load_one_to_many_mappings_and_indices(data_configuration[ONE_TO_MANY_RELATIONS_KEY], data_configuration.get(INDICES_KEY, {}))
+        self.__load_many_to_many_mappings(data_configuration.get(MANY_TO_MANY_RELATIONS_KEY, {}))
 
     def __load_name_mappings(self, name_mappings: Dict[str, Any]):
         self._configurations = {k: Configuration(**v) for k, v in name_mappings.items()}
 
     def __load_one_to_many_mappings_and_indices(
-        self, 
-        one_to_many_configuration: Dict[str, List[str]], 
+        self,
+        one_to_many_configuration: Dict[str, List[str]],
         indices: Dict[str, List[str]]
     ) -> None:
         self._one_to_many_mappings = [
             TableMapping(
-                table_name=table_name, 
-                mapping=[OneToManyMapping(mapping=ForeignKeyMapping(**relation)) for relation in relations], 
+                table_name=table_name,
+                mapping=[OneToManyMapping(mapping=ForeignKeyMapping(**relation)) for relation in relations],
                 indices=indices[table_name]
             ) for table_name, relations in one_to_many_configuration.items()]
 
     def __load_many_to_many_mappings(self, many_to_many_configuration: Dict[str, Any]):
         self._many_to_many_mappings = [
             TableMapping(
-            table_name=table_name, 
+            table_name=table_name,
                 mapping=ManyToManyMapping(
                     mapping1=ForeignKeyMapping(**relations["mapping1"]),
                     mapping2=ForeignKeyMapping(**relations["mapping2"]),
@@ -404,53 +429,3 @@ class TableToGraphImporter:
                 )
             )
             for table_name, relations in many_to_many_configuration.items()]
-
-
-class S3Importer(TableToGraphImporter):
-    def __init__(
-        self, 
-        bucket_name: str,
-        s3_access_key: str,
-        s3_secret_key: str,
-        s3_region: str,
-        data_configuration: Dict[str, Any],
-        file_extension: str, 
-        s3_session_token: Optional[str] = None,
-        memgraph: Optional[Memgraph] = None
-    ) -> None:
-        super().__init__(
-            data_source=S3DataSource(
-                bucket_name=bucket_name,
-                s3_access_key=s3_access_key,
-                s3_secret_key=s3_secret_key,
-                s3_region=s3_region,
-                s3_session_token=s3_session_token,
-                file_extension=file_extension
-            ),
-            data_configuration=data_configuration,
-            memgraph=memgraph
-        )
-
-
-class AzureBlobImporter(TableToGraphImporter):
-    def __init__(
-        self,
-        container_name: str,
-        account_name: str,
-        account_key: str,
-        file_extension: str,
-        data_configuration: Dict[str, Any],
-        sas_token: Optional[str] = None,
-        memgraph: Optional[Memgraph] = None
-    ) -> None:
-        super().__init__(
-            data_source=AzureBlobDataSource(
-                container_name=container_name,
-                account_name=account_name,
-                account_key=account_key,
-                file_extension=file_extension,
-                sas_token=sas_token
-            ),
-            data_configuration=data_configuration,
-            memgraph=memgraph
-        )
