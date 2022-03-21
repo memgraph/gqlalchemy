e8b8e7091c9ccb4c38f7ede9bd77dabfa06ee981
katarinasupe <supe.katarina@gmail.com>
2022-03-21 17:05:14 +0100

    Extend test

--- tests/ogm/test_properties.py
+++ tests/ogm/test_properties.py
@@ -6,15 +6,33 @@ from gqlalchemy import Node
 def test_private_properties(memgraph):
     class User(Node):
         id: int
+        last_name: str
         _name: str
+        _age: int
 
-    user = User(id=1, _name="Jane").save(memgraph)
+    user = User(id=1, last_name="Smith", _name="Jane").save(memgraph)
+    User(id=2, last_name="Scott").save(memgraph)
     loaded_user = memgraph.load_node(user)
+    loaded_user._age = 24
+    try:
+        memgraph.load_node(User(id=2))
+    except ValidationError:
+        loaded_user2 = memgraph.load_node(User(id=2, last_name="Scott"))
 
+    assert type(loaded_user) is User
+    assert type(loaded_user2) is User
     assert hasattr(loaded_user, "_name") is False
+    assert hasattr(loaded_user, "_age") is True
+    assert hasattr(loaded_user2, "_name") is False
+    assert hasattr(loaded_user2, "_age") is False
     assert loaded_user.id == 1
+    assert loaded_user.last_name == "Smith"
     assert loaded_user._label == "User"
-    assert user._name == "Jane"
+    assert loaded_user2.id == 2
+    assert loaded_user2.last_name == "Scott"
+    assert loaded_user2._label == "User"
+    user._name == "Jane"
+    assert loaded_user._age == 24
 
 
 # test whether the node can be loaded with one of the properties
