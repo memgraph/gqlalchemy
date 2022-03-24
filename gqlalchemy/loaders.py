1ef228ed16f905d2c1c51fdd3eee554411925c13
Boris Tasevski <boris.tasevski@memgraph.io>
2022-03-24 12:24:47 +0100

    Format document

--- gqlalchemy/loaders.py
+++ gqlalchemy/loaders.py
@@ -20,20 +20,14 @@ from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from dacite import from_dict
 from gqlalchemy.models import (
-    MemgraphIndex, 
-    MemgraphTrigger, 
-    TriggerEventObject, 
-    TriggerEventType, 
-    TriggerExecutionPhase
+    MemgraphIndex,
+    MemgraphTrigger,
+    TriggerEventObject,
+    TriggerEventType,
+    TriggerExecutionPhase,
 )
 from pyarrow import fs
-from typing import (
-    List, 
-    Dict, 
-    Any, 
-    Optional,
-    Union
-)
+from typing import List, Dict, Any, Optional, Union
 import pyarrow.dataset as ds
 
 
@@ -58,6 +52,7 @@ class ForeignKeyMapping:
     :param reference_table: Name of a table from which the foreign key is taken
     :param reference_key: Column name in referenced table from which the foreign key is taken
     """
+
     column_name: str
     reference_table: str
     reference_key: str
@@ -73,6 +68,7 @@ class OneToManyMapping:
     :param from_entity: Direction of the relationship created from mapping object
     :param variables: Variables that will be added to the relationship created from this object (Optional)
     """
+
     foreign_key: ForeignKeyMapping
     label: str
     from_entity: bool = False
@@ -90,6 +86,7 @@ class ManyToManyMapping:
     :param label: Label to be applied to the newly created relationship
     :param variables: Variables that will be added to the relationship created from this object (Optional)
     """
+
     foreign_key_from: ForeignKeyMapping
     foreign_key_to: ForeignKeyMapping
     label: str
@@ -108,6 +105,7 @@ class TableMapping:
     :param mapping: All of the mappings in the table (Optional)
     :param indices: List of the indices to be created for this table (Optional)
     """
+
     table_name: str
     mapping: Optional[Mapping] = None
     indices: Optional[List[str]] = None
@@ -121,6 +119,7 @@ class NameMappings:
     :param label: New label (Optional)
     :param column_names_mapping: Dictionary containing key-value pairs in form ("column name", "property name") (Optional)
     """
+
     label: Optional[str] = None
     column_names_mapping: Dict[str, str] = field(default_factory=dict)
 
@@ -132,6 +131,7 @@ class DataSource(ABC):
     """
     Base class. Inherit this class and encapsulate reading of the file format from some data source
     """
+
     def __init__(self, file_extension: str) -> None:
         """
         :param file_extension: Extension for which the reading is implemented
@@ -139,11 +139,7 @@ class DataSource(ABC):
         self._file_extension: str = file_extension
 
     @abstractmethod
-    def load_data(
-        self,
-        collection_name: str,
-        is_cross_table: bool=False
-    ) -> None:
+    def load_data(self, collection_name: str, is_cross_table: bool = False) -> None:
         """
         Override this method in the derrived class. Intended to be used for reading data from data format.
 
@@ -157,8 +153,9 @@ class S3DataSource(DataSource):
     """
     Hold the implementation of reading files from Amazon S3
     """
+
     def __init__(
-        self, 
+        self,
         bucket_name: str,
         s3_access_key: str,
         s3_secret_key: str,
@@ -188,7 +185,7 @@ class S3DataSource(DataSource):
         columns: Optional[List[str]] = None,
     ) -> None:
         """
-        Read the data from S3 and process it in batches. 
+        Read the data from S3 and process it in batches.
 
         :param collection_name: Name of the collection from which to read the data
         :param is_cross_table: Indicate whether or not the collection contains associative table (default=False)
@@ -204,10 +201,7 @@ class S3DataSource(DataSource):
         print("Loading " + ("cross table " if is_cross_table else "") + f"data from {source}")
 
         # Load dataset via Pyarrow
-        dataset = ds.dataset(
-            source=source, 
-            filesystem=s3
-        )
+        dataset = ds.dataset(source=source, filesystem=s3)
 
         # Load batches from raw data
         for batch in dataset.to_batches(
@@ -221,6 +215,7 @@ class NameMapper:
     """
     Class that holds all name mappings for all of the collections.
     """
+
     def __init__(self, mappings: Dict[str, Any]) -> None:
         self._name_mappings: Dict[str, NameMappings] = {k: NameMappings(**v) for k, v in mappings.items()}
 
@@ -253,31 +248,27 @@ class TableToGraphImporter:
     """
     Class that implements translation of table data to graph data, and imports it to Memgraph
     """
+
     _DIRECTION = {
         True: (NODE_A, NODE_B),
         False: (NODE_B, NODE_A),
     }
 
     _TriggerQueryTemplate = Template(
-        Unwind(list_expression="createdVertices", variable="$node_a") \
-            .with_(results={"$node_a":""}) \
-            .where(property1="$node_a:$label_2", operator="MATCH", property2="($node_b:$label_1)") \
-            .where(property1="$node_b.$property_1", operator="=", property2="$node_a.$property_2") \
-            .create() \
-            .node(variable="$from_node") \
-            .to(edge_label="$edge_type") \
-            .node(variable="$to_node") \
-            .construct_query()
+        Unwind(list_expression="createdVertices", variable="$node_a")
+        .with_(results={"$node_a": ""})
+        .where(property1="$node_a:$label_2", operator="MATCH", property2="($node_b:$label_1)")
+        .where(property1="$node_b.$property_1", operator="=", property2="$node_a.$property_2")
+        .create()
+        .node(variable="$from_node")
+        .to(edge_label="$edge_type")
+        .node(variable="$to_node")
+        .construct_query()
     )
 
     @staticmethod
     def _create_trigger_cypher_query(
-        label1: str, 
-        label2: str, 
-        property1: str, 
-        property2: str, 
-        edge_type: str, 
-        from_entity: bool
+        label1: str, label2: str, property1: str, property2: str, edge_type: str, from_entity: bool
     ) -> str:
         """
         Creates Cypher Query for translation Trigger
@@ -304,10 +295,7 @@ class TableToGraphImporter:
         )
 
     def __init__(
-        self,
-        data_source: DataSource,
-        data_configuration: Dict[str, Any],
-        memgraph: Optional[Memgraph] = None
+        self, data_source: DataSource, data_configuration: Dict[str, Any], memgraph: Optional[Memgraph] = None
     ) -> None:
         """
         :param data_source: Source of the data
@@ -322,7 +310,7 @@ class TableToGraphImporter:
     def translate(self, drop_database_on_start: bool) -> None:
         """
         Performs the translations
-        
+
         :param drop_database_on_start: Indicate whether or not the database should be dropped prior to the start of the translations
         """
         if drop_database_on_start:
@@ -343,10 +331,7 @@ class TableToGraphImporter:
         for one_to_many_mapping in self._one_to_many_mappings:
             collection_name = one_to_many_mapping.table_name
             for row in self._data_source.load_data(collection_name=collection_name):
-                self._save_row_as_node(
-                    label=collection_name, 
-                    row=row
-                )
+                self._save_row_as_node(label=collection_name, row=row)
 
     def _load_cross_relationships(self) -> None:
         """
@@ -356,14 +341,16 @@ class TableToGraphImporter:
             mapping_from = many_to_many_mapping.mapping.foreign_key_from
             mapping_to = many_to_many_mapping.mapping.foreign_key_to
 
-            for row in self._data_source.load_data(collection_name=many_to_many_mapping.table_name, is_cross_table=True):
+            for row in self._data_source.load_data(
+                collection_name=many_to_many_mapping.table_name, is_cross_table=True
+            ):
                 self._save_row_as_relationship(
-                    collection_name_from=mapping_from.reference_table, 
+                    collection_name_from=mapping_from.reference_table,
                     collection_name_to=mapping_to.reference_table,
-                    property_from=mapping_from.reference_key, 
+                    property_from=mapping_from.reference_key,
                     property_to=mapping_to.reference_key,
                     relation_label=many_to_many_mapping.mapping.label,
-                    row=row
+                    row=row,
                 )
 
     def _create_triggers(self) -> None:
@@ -377,47 +364,37 @@ class TableToGraphImporter:
             label1 = self._name_mapper.get_label(collection_name=one_to_many_mapping.table_name)
             for mapping in one_to_many_mapping.mapping:
                 property1 = self._name_mapper.get_property_name(
-                    collection_name=one_to_many_mapping.table_name, 
-                    column_name=mapping.foreign_key.column_name
+                    collection_name=one_to_many_mapping.table_name, column_name=mapping.foreign_key.column_name
                 )
-                label2 = self._name_mapper.get_label(
-                    collection_name=mapping.foreign_key.reference_table
-                )
+                label2 = self._name_mapper.get_label(collection_name=mapping.foreign_key.reference_table)
                 property2 = self._name_mapper.get_property_name(
-                    collection_name=one_to_many_mapping.table_name, 
-                    column_name=mapping.foreign_key.reference_key
+                    collection_name=one_to_many_mapping.table_name, column_name=mapping.foreign_key.reference_key
                 )
                 edge_type = mapping.label
                 from_entity = mapping.from_entity
 
                 self._create_trigger(
-                    label1=label1, 
-                    label2=label2, 
-                    property1=property1, 
-                    property2=property2, 
-                    edge_type=edge_type, 
-                    from_entity=from_entity
+                    label1=label1,
+                    label2=label2,
+                    property1=property1,
+                    property2=property2,
+                    edge_type=edge_type,
+                    from_entity=from_entity,
                 )
                 self._create_trigger(
-                    label1=label2, 
-                    label2=label1, 
-                    property1=property2, 
-                    property2=property1, 
-                    edge_type=edge_type, 
-                    from_entity=not from_entity
+                    label1=label2,
+                    label2=label1,
+                    property1=property2,
+                    property2=property1,
+                    edge_type=edge_type,
+                    from_entity=not from_entity,
                 )
 
     def _create_trigger(
-        self, 
-        label1: str, 
-        label2: str, 
-        property1: str, 
-        property2: str, 
-        edge_type: str, 
-        from_entity: bool
+        self, label1: str, label2: str, property1: str, property2: str, edge_type: str, from_entity: bool
     ) -> None:
         """
-        Creates translation trigger in Memgraph. 
+        Creates translation trigger in Memgraph.
 
         :param label1: Label of the first Node
         :param label2: Label of the second Node
@@ -433,7 +410,9 @@ class TableToGraphImporter:
             event_type=TriggerEventType.CREATE,
             event_object=TriggerEventObject.NODE,
             execution_phase=TriggerExecutionPhase.BEFORE,
-            statement=TableToGraphImporter._create_trigger_cypher_query(label1, label2, property1, property2, edge_type, from_entity)
+            statement=TableToGraphImporter._create_trigger_cypher_query(
+                label1, label2, property1, property2, edge_type, from_entity
+            ),
         )
         print(f"Created trigger {trigger_name}")
 
@@ -447,15 +426,14 @@ class TableToGraphImporter:
             collection_name = self._name_mapper.get_label(collection_name=one_to_many_mapping.table_name)
             for index in one_to_many_mapping.indices:
                 new_index = self._name_mapper.get_property_name(
-                    collection_name=one_to_many_mapping.table_name, 
-                    column_name=index
+                    collection_name=one_to_many_mapping.table_name, column_name=index
                 )
                 self._memgraph.create_index(index=MemgraphIndex(collection_name, new_index))
                 print(f"Created index for {collection_name} on {new_index}")
 
     def _save_row_as_node(
-        self, 
-        label: str, 
+        self,
+        label: str,
         row: Dict[str, Any],
     ) -> None:
         """
@@ -465,22 +443,24 @@ class TableToGraphImporter:
         :param row: Row that should be saved to Memgraph as Node
         """
         list(
-            QueryBuilder(connection=self._memgraph).
-            create().
-            node(
-                labels=self._name_mapper.get_label(collection_name=label), 
-                **{self._name_mapper.get_property_name(collection_name=label, column_name=k): v for k, v in row.items()}
+            QueryBuilder(connection=self._memgraph)
+            .create()
+            .node(
+                labels=self._name_mapper.get_label(collection_name=label),
+                **{
+                    self._name_mapper.get_property_name(collection_name=label, column_name=k): v for k, v in row.items()
+                },
             )
             .execute()
         )
 
     def _save_row_as_relationship(
-        self, 
+        self,
         collection_name_from: str,
         collection_name_to: str,
         property_from: str,
         property_to: str,
-        relation_label: str, 
+        relation_label: str,
         row: Dict[str, Any],
     ) -> None:
         """
@@ -497,14 +477,13 @@ class TableToGraphImporter:
             QueryBuilder(connection=self._memgraph)
             .match()
             .node(
-                labels=self._name_mapper.get_label(collection_name=collection_name_from), 
+                labels=self._name_mapper.get_label(collection_name=collection_name_from),
                 variable=NODE_A,
                 **{
                     self._name_mapper.get_property_name(
-                        collection_name=collection_name_from,
-                        column_name=property_from
+                        collection_name=collection_name_from, column_name=property_from
                     ): row[property_from]
-                }
+                },
             )
             .match()
             .node(
@@ -512,10 +491,9 @@ class TableToGraphImporter:
                 variable=NODE_B,
                 **{
                     self._name_mapper.get_property_name(
-                        collection_name=collection_name_to,
-                        column_name=property_to
+                        collection_name=collection_name_to, column_name=property_to
                     ): row[property_to]
-                }
+                },
             )
             .create()
             .node(variable=NODE_A)
@@ -525,13 +503,14 @@ class TableToGraphImporter:
             .execute()
         )
 
-
     def __load_configuration(self, data_configuration: Dict[str, Any]) -> None:
         """
         Loads all of the configuration
         """
         self.__load_name_mappings(data_configuration.get(NAME_MAPPINGS_KEY, {}))
-        self.__load_one_to_many_mappings_and_indices(data_configuration[ONE_TO_MANY_RELATIONS_KEY], data_configuration.get(INDICES_KEY, {}))
+        self.__load_one_to_many_mappings_and_indices(
+            data_configuration[ONE_TO_MANY_RELATIONS_KEY], data_configuration.get(INDICES_KEY, {})
+        )
         self.__load_many_to_many_mappings(data_configuration.get(MANY_TO_MANY_RELATIONS_KEY, {}))
 
     def __load_name_mappings(self, name_mappings: Dict[str, Any]) -> None:
@@ -541,19 +520,17 @@ class TableToGraphImporter:
         self._name_mapper = NameMapper(mappings=name_mappings)
 
     def __load_one_to_many_mappings_and_indices(
-        self, 
-        one_to_many_configuration: Dict[str, List[str]], 
-        indices: Dict[str, List[str]]
+        self, one_to_many_configuration: Dict[str, List[str]], indices: Dict[str, List[str]]
     ) -> None:
         """
-        Loads One To Many Mappings and indices from the configuration. 
+        Loads One To Many Mappings and indices from the configuration.
         """
         self._one_to_many_mappings = [
             TableMapping(
-                table_name=table_name, 
-                mapping=[from_dict(data_class=OneToManyMapping, data=relation) for relation in relations], 
-                indices=indices.get(table_name, {})
-            ) 
+                table_name=table_name,
+                mapping=[from_dict(data_class=OneToManyMapping, data=relation) for relation in relations],
+                indices=indices.get(table_name, {}),
+            )
             for table_name, relations in one_to_many_configuration.items()
         ]
 
@@ -562,25 +539,22 @@ class TableToGraphImporter:
         Loads Many To Many Mappings from the configuration
         """
         self._many_to_many_mappings = [
-            TableMapping(
-                table_name=table_name, 
-                mapping=from_dict(data_class=ManyToManyMapping, data=relations)
-            )
+            TableMapping(table_name=table_name, mapping=from_dict(data_class=ManyToManyMapping, data=relations))
             for table_name, relations in many_to_many_configuration.items()
         ]
 
 
 class S3Translator(TableToGraphImporter):
     def __init__(
-        self, 
+        self,
         bucket_name: str,
         s3_access_key: str,
         s3_secret_key: str,
         s3_region: str,
         data_configuration: Dict[str, Any],
-        file_extension: str, 
+        file_extension: str,
         s3_session_token: Optional[str] = None,
-        memgraph: Optional[Memgraph] = None
+        memgraph: Optional[Memgraph] = None,
     ) -> None:
         super().__init__(
             data_source=S3DataSource(
@@ -589,8 +563,8 @@ class S3Translator(TableToGraphImporter):
                 s3_secret_key=s3_secret_key,
                 s3_region=s3_region,
                 s3_session_token=s3_session_token,
-                file_extension=file_extension
+                file_extension=file_extension,
             ),
             data_configuration=data_configuration,
-            memgraph=memgraph
+            memgraph=memgraph,
         )
