0fb7a3d26a9013a5dca0496022198886e3546fff
Katarina Supe <61758502+katarinasupe@users.noreply.github.com>
2022-03-31 09:11:09 +0200

    [develop < T0040-GA] Add index on label (#113)
    * Add new exception class for index on label
    
    * Add index on label with tests
    
    * Add new lines
    
    * Add newlines again
    
    * Change test according to docs
    
    * Remove None from attrs.get

--- gqlalchemy/models.py
+++ gqlalchemy/models.py
@@ -24,6 +24,7 @@ from .exceptions import (
     GQLAlchemyError,
     GQLAlchemySubclassNotFoundWarning,
     GQLAlchemyDatabaseMissingInFieldError,
+    GQLAlchemyDatabaseMissingInNodeClassError,
 )
 
 
@@ -187,7 +188,7 @@ class GraphObject(BaseModel):
     class Config:
         extra = Extra.allow
 
-    def __init_subclass__(cls, type=None, label=None, labels=None):
+    def __init_subclass__(cls, type=None, label=None, labels=None, index=None, db=None):
         """Stores the subclass by type if type is specified, or by class name
         when instantiating a subclass.
         """
@@ -355,7 +356,9 @@ class NodeMetaclass(BaseModel.__class__):
             return None
 
         cls = super().__new__(mcs, name, bases, namespace, **kwargs)
+        cls.index = kwargs.get("index")
         cls.label = kwargs.get("label", name)
+
         if name == "Node":
             pass
         elif "labels" in kwargs:  # overrides superclass labels
@@ -365,13 +368,24 @@ class NodeMetaclass(BaseModel.__class__):
         else:
             cls.labels = {cls.label}
 
+        db = kwargs.get("db")
+
+        if cls.index is True:
+            if db is None:
+                raise GQLAlchemyDatabaseMissingInNodeClassError(cls=cls)
+
+            index = MemgraphIndex(cls.label)
+            db.create_index(index)
+
         for field in cls.__fields__:
             attrs = cls.__fields__[field].field_info.extra
             field_type = cls.__fields__[field].type_.__name__
-
             label = attrs.get("label", cls.label)
-            db = attrs.get("db", None)
             skip_constraints = False
+
+            if db is None:
+                db = attrs.get("db")
+
             for constraint in FieldAttrsConstants.list():
                 if constraint in attrs and db is None:
                     base = field_in_superclass(field, constraint)
@@ -379,6 +393,7 @@ class NodeMetaclass(BaseModel.__class__):
                         cls.__fields__[field].field_info.extra = base.__fields__[field].field_info.extra
                         skip_constraints = True
                         break
+
                     raise GQLAlchemyDatabaseMissingInFieldError(
                         constraint=constraint,
                         field=field,
@@ -402,6 +417,7 @@ class NodeMetaclass(BaseModel.__class__):
 
             if attrs and "db" in attrs:
                 del attrs["db"]
+
         return cls
 
 
