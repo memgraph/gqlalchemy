a41c2f3f31d862839ba1fd6a236445832ec5bbe1
Boris Tasevski <boris.tasevski@memgraph.io>
2022-04-08 18:44:42 +0200

    unused imports removed

--- gqlalchemy/query_builder.py
+++ gqlalchemy/query_builder.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from dataclasses import dataclass
 from enum import Enum
 import re
 from abc import ABC, abstractmethod
