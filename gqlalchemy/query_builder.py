0b9d5a5d88eee31e0fe52a6af97354274e981958
Katarina Supe <61758502+katarinasupe@users.noreply.github.com>
2022-05-18 10:20:49 +0200

    [develop < T0081-GA] Rename Edge to Relationship (#145)
    * Rename edge to relationship; add kwargs in tests
    
    * Fix test on docs
    
    * [develop < TOO53-GA] Investigate Timeout error (#120)
    
    * Update dependencies
    
    * Increase Bolt session inactivity timeout
    
    * Update workflow
    
    * Update workflow
    
    * Add config flag --also-log-to-stderr
    
    * Catch Memgraph logs
    
    * Catch Memgraph logs
    
    * Update log level
    
    * Upload log as artifact
    
    * Add log file
    
    * Create new log directory
    
    * Run Action step even if error is thrown
    
    * Change session nactivity timeout
    
    * Change Ubuntu GitHub job
    
    * Update Ubuntu GitHub job
    
    * Update Ubuntu GitHub job
    
    * Update Ubuntu GitHub job
    
    * Update Ubuntu GitHub job
    
    * Update GitHub action paths
    
    * Update GitHub action paths
    
    * Update GitHub action permissions
    
    * Update GitHub action
    
    * Update GitHub action
    
    * Update GitHub action
    
    * Update GitHub action
    
    * Update GitHub action
    
    * Update GitHub action
    
    * [develop < T0070-GA] Add more clauses for base classes (#139)
    
    * added load_csv, tests
    
    * add return base class, change CSV to Csv
    
    * black formatting
    
    * Change edge in loaders
    
    * Change labels_str -> type_str; edge_label-> relationship_type
    
    Co-authored-by: g-despot <66276597+g-despot@users.noreply.github.com>
    Co-authored-by: Bruno Sačarić <bruno.sacaric@gmail.com>

--- gqlalchemy/query_builder.py
+++ gqlalchemy/query_builder.py
@@ -34,7 +34,7 @@ class DeclarativeBaseTypes:
     CALL = "CALL"
     CREATE = "CREATE"
     DELETE = "DELETE"
-    EDGE = "EDGE"
+    RELATIONSHIP = "RELATIONSHIP"
     LIMIT = "LIMIT"
     LOAD_CSV = "LOAD_CSV"
     MATCH = "MATCH"
@@ -244,7 +244,7 @@ class NodePartialQuery(PartialQuery):
         return f"({self.variable}{self.labels}{' ' + self.properties if self.properties else ''})"
 
 
-class EdgePartialQuery(PartialQuery):
+class RelationshipPartialQuery(PartialQuery):
     def __init__(
         self,
         variable: Optional[str],
@@ -254,7 +254,7 @@ class EdgePartialQuery(PartialQuery):
         directed: bool,
         from_: bool,
     ):
-        super().__init__(DeclarativeBaseTypes.EDGE)
+        super().__init__(DeclarativeBaseTypes.RELATIONSHIP)
 
         self.directed = directed
         self._variable = variable
@@ -280,7 +280,7 @@ class EdgePartialQuery(PartialQuery):
         return "" if self._properties is None else self._properties
 
     def construct_query(self) -> str:
-        """Constructs an edge partial query."""
+        """Constructs an relationship partial query."""
         relationship_query = f"{self.variable}{self.labels}{self.algorithm}{self.properties}"
 
         if not self.directed:
@@ -599,7 +599,7 @@ class DeclarativeBase(ABC):
 
     def to(
         self,
-        edge_label: Optional[str] = "",
+        relationship_type: Optional[str] = "",
         directed: Optional[bool] = True,
         variable: Optional[str] = None,
         relationship: Optional["Relationship"] = None,
@@ -609,7 +609,7 @@ class DeclarativeBase(ABC):
         """Add a relationship pattern to the query.
 
         Args:
-            edge_label: A string representing the type of the relationship.
+            relationship_type: A string representing the type of the relationship.
             directed: A bool indicating if the relationship is directed.
             variable: A string representing the name of the variable for storing
               results of the relationship pattern.
@@ -620,18 +620,18 @@ class DeclarativeBase(ABC):
         Returns:
             A `DeclarativeBase` instance for constructing queries.
         """
-        if not self._is_linking_valid_with_query(DeclarativeBaseTypes.EDGE):
+        if not self._is_linking_valid_with_query(DeclarativeBaseTypes.RELATIONSHIP):
             raise InvalidMatchChainException()
 
         if relationship is None:
-            type_str = to_cypher_labels(edge_label)
+            type_str = to_cypher_labels(relationship_type)
             properties_str = to_cypher_properties(kwargs)
         else:
             type_str = to_cypher_labels(relationship._type)
             properties_str = to_cypher_properties(relationship._properties)
 
         self._query.append(
-            EdgePartialQuery(
+            RelationshipPartialQuery(
                 variable=variable,
                 labels=type_str,
                 algorithm="" if algorithm is None else str(algorithm),
@@ -645,7 +645,7 @@ class DeclarativeBase(ABC):
 
     def from_(
         self,
-        edge_label: Optional[str] = "",
+        relationship_type: Optional[str] = "",
         directed: Optional[bool] = True,
         variable: Optional[str] = None,
         relationship: Optional["Relationship"] = None,
@@ -655,7 +655,7 @@ class DeclarativeBase(ABC):
         """Add a relationship pattern to the query.
 
         Args:
-            edge_label: A string representing the type of the relationship.
+            relationship_type: A string representing the type of the relationship.
             directed: A bool indicating if the relationship is directed.
             variable: A string representing the name of the variable for storing
               results of the relationship pattern.
@@ -665,20 +665,20 @@ class DeclarativeBase(ABC):
         Returns:
             A `DeclarativeBase` instance for constructing queries.
         """
-        if not self._is_linking_valid_with_query(DeclarativeBaseTypes.EDGE):
+        if not self._is_linking_valid_with_query(DeclarativeBaseTypes.RELATIONSHIP):
             raise InvalidMatchChainException()
 
         if relationship is None:
-            labels_str = to_cypher_labels(edge_label)
+            type_str = to_cypher_labels(relationship_type)
             properties_str = to_cypher_properties(kwargs)
         else:
-            labels_str = to_cypher_labels(relationship._type)
+            type_str = to_cypher_labels(relationship._type)
             properties_str = to_cypher_properties(relationship._properties)
 
         self._query.append(
-            EdgePartialQuery(
+            RelationshipPartialQuery(
                 variable=variable,
-                labels=labels_str,
+                labels=type_str,
                 algorithm="" if algorithm is None else str(algorithm),
                 properties=properties_str,
                 directed=bool(directed),
@@ -1182,7 +1182,7 @@ class DeclarativeBase(ABC):
     def _any_variables_matched(self) -> bool:
         """Checks if any variables are present in the result."""
         return any(
-            q.type in [DeclarativeBaseTypes.EDGE, DeclarativeBaseTypes.NODE] and q.variable not in [None, ""]
+            q.type in [DeclarativeBaseTypes.RELATIONSHIP, DeclarativeBaseTypes.NODE] and q.variable not in [None, ""]
             for q in self._query
         )
 
