479e1371479912c1bfabeb4f6c6ddccc7feea2a6
Bruno Sačarić <bruno.sacaric@gmail.com>
2022-05-18 10:50:11 +0200

    moved integrated algorithms (#147)

--- gqlalchemy/query_builder.py
+++ gqlalchemy/query_builder.py
@@ -17,7 +17,8 @@ import re
 from abc import ABC, abstractmethod
 from typing import Any, Dict, Iterator, List, Optional, Tuple, Union
 
-from .memgraph import Connection, Memgraph, IntegratedAlgorithm
+from .memgraph import Connection, Memgraph
+from .graph_algorithms.integrated_algorithms import IntegratedAlgorithm
 from .utilities import to_cypher_labels, to_cypher_properties, to_cypher_value
 from .models import Node, Relationship
 from .exceptions import (
