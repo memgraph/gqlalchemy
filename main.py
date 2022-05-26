9cf035c242b8f65a2363922c6f1b6263e52f94e1
niko4299 <niko.krvavica@gmail.com>
2022-05-26 15:21:04 +0200

    test

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ if __name__ == "__main__":
     mg = Memgraph()
 
     qb = QueryBuilder()
-    qb = QueryBuilder()
 
     file_text = open('gqlalchemy/query_modules/push_streams/kafka.py','r').read()
-    query = "mg.create_module_file", "'gov.py', '{file_text}'".format(file_text = file_text)
-    print(query)
+    query = "mg.create_module_file", "'gov.py', 'kk'"
+      
+    query = "CALL mg.create_module_file('gov.py','kk');"
+    # query = qb.call("mg.create_module_file", "'govnich.py', 'Hello soul sister!'").yield_().construct_query()
+    # result = list(mg.execute_and_fetch(query))
     query = qb.call(query).yield_().construct_query()
     result = list(mg.execute_and_fetch(query))
     result = list(mg.execute_and_fetch("CALL mg.procedures() YIELD *"))
