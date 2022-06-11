5702b35e4284852c9fef4994b19931016ccbb71e
Bruno Sacaric <bruno.sacaric@gmail.com>
2022-06-11 13:03:36 +0200

    I see no changes, wake up in the morning and I ask myself

--- gqlalchemy/query_builder.py
+++ gqlalchemy/query_builder.py
@@ -245,7 +245,7 @@ class XorNotWhereConditionPartialQuery(WhereNotConditionPartialQuery):
 
 
 class NodePartialQuery(PartialQuery):
-    def __init__(self, variable: str, labels: str, properties: str):
+    def __init__(self, variable: Optional[str], labels: str, properties: str):
         super().__init__(DeclarativeBaseTypes.NODE)
 
         self._variable = variable
@@ -254,15 +254,15 @@ class NodePartialQuery(PartialQuery):
 
     @property
     def variable(self) -> str:
-        return self._variable if self._variable is not None else ""
+        return "" if self._variable is None else self._variable
 
     @property
     def labels(self) -> str:
-        return self._labels if self._labels is not None else ""
+        return self._labels
 
     @property
     def properties(self) -> str:
-        return self._properties if self._properties is not None else ""
+        return self._properties
 
     def construct_query(self) -> str:
         """Constructs a node partial query."""
@@ -273,9 +273,9 @@ class RelationshipPartialQuery(PartialQuery):
     def __init__(
         self,
         variable: Optional[str],
-        labels: Optional[str],
-        algorithm: Optional[str],
-        properties: Optional[str],
+        labels: str,
+        algorithm: str,
+        properties: str,
         directed: bool,
         from_: bool,
     ):
@@ -294,18 +294,18 @@ class RelationshipPartialQuery(PartialQuery):
 
     @property
     def labels(self) -> str:
-        return "" if self._labels is None else self._labels
+        return self._labels
 
     @property
     def algorithm(self) -> str:
-        return "" if self._algorithm is None else self._algorithm
+        return self._algorithm
 
     @property
     def properties(self) -> str:
-        return "" if self._properties is None else self._properties
+        return self._properties
 
     def construct_query(self) -> str:
-        """Constructs an relationship partial query."""
+        """Constructs a relationship partial query."""
         relationship_query = f"{self.variable}{self.labels}{self.algorithm}{self.properties}"
 
         if not self.directed:
@@ -446,7 +446,7 @@ class DeletePartialQuery(PartialQuery):
 
     @property
     def variable_expressions(self) -> str:
-        return self._variable_expressions if self._variable_expressions is not None else ""
+        return self._variable_expressions
 
     def construct_query(self) -> str:
         """Creates a DELETE statement Cypher partial query."""
@@ -461,7 +461,7 @@ class RemovePartialQuery(PartialQuery):
 
     @property
     def items(self) -> str:
-        return self._items if self._items is not None else ""
+        return self._items
 
     def construct_query(self) -> str:
         """Creates a REMOVE statement Cypher partial query."""
