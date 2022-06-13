4c6291ceafd89ec03137db20bbedea7974e2b013
josipmrden <josip.mrden@external-basf.com>
2022-06-13 11:41:36 +0200

    Format imports

--- gqlalchemy/query_builder.py
+++ gqlalchemy/query_builder.py
@@ -16,6 +16,7 @@ import re
 from abc import ABC, abstractmethod
 from enum import Enum
 from typing import Any, Dict, Iterable, Iterator, List, Optional, Tuple, Union, Set
+
 from .memgraph import Connection, Memgraph
 from .graph_algorithms.integrated_algorithms import IntegratedAlgorithm
 from .utilities import to_cypher_labels, to_cypher_properties, to_cypher_value, to_cypher_qm_arguments
