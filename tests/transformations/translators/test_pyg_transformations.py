b91af788f37ecc1be2cfedc2bc4500f690b867d4
antepusic <ante.pusic@fer.hr>
2023-04-18 11:39:09 +0200

    Fix import

--- tests/transformations/translators/test_pyg_transformations.py
+++ tests/transformations/translators/test_pyg_transformations.py
@@ -18,12 +18,12 @@ from typing import Dict, Any, Set
 
 from gqlalchemy import Match
 from gqlalchemy.models import Node, Relationship
-from gqlalchemy.transformations.translators.pyg_translator import PyGTranslator
 from gqlalchemy.transformations.translators.translator import Translator
 from gqlalchemy.transformations.constants import PYG_ID, DEFAULT_NODE_LABEL, DEFAULT_EDGE_TYPE
 from gqlalchemy.utilities import to_cypher_value
 from tests.transformations.common import execute_queries
 
+PyGTranslator = pytest.importorskip("gqlalchemy.transformations.translators.pyg_translator.PyGTranslator")
 Data = pytest.importorskip("torch_geometric.data.Data")
 HeteroData = pytest.importorskip("torch_geometric.data.HeteroData")
 FakeDataset = pytest.importorskip("torch_geometric.datasets.FakeDataset")
